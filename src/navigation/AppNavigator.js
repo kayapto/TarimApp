8aa53f12900a7eb338d712a1f6001ead11b0fc92
kayapto <166363216+kayapto@users.noreply.github.com>
2024-08-02 16:07:05 -0400

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import NewAdScreen from '../screens/NewAdScreen';
+import CartScreen from '../screens/CartScreen';
+import ChatScreen from '../screens/ChatScreen';
+import OrderScreen from '../screens/OrderScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="NewAd" component={NewAdScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Order" component={OrderScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
